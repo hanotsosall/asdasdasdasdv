de26525f9b5eb3944efba98012402683e9153ad2
hanotsosall <dmitrijkucenko332@gmail.com>
2026-05-26 20:34:09 +0300

    Update groq_client.py

--- utils/groq_client.py
+++ utils/groq_client.py
@@ -1,6 +1,14 @@
 import groq
 from config import GROQ_API_KEY
 
+import os
+
+# Убираем прокси-переменные, чтобы httpx их не подхватил
+os.environ.pop("HTTP_PROXY", None)
+os.environ.pop("HTTPS_PROXY", None)
+os.environ.pop("http_proxy", None)
+os.environ.pop("https_proxy", None)
+
 client = groq.Groq(api_key=GROQ_API_KEY)
 
 def ask_groq(prompt: str) -> str:
